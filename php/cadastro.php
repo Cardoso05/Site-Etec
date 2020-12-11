7169855c9be853180b6d7b9402898ed1d4d3a5bc
Cardoso05 <zekkacardoso18@gmail.com>
2020-12-11 12:22:01 -0300

    cadastro

--- php/cadastro.php
+++ php/cadastro.php
@@ -12,7 +12,34 @@ $sql = "select login from usuarios where login = '$login'";
 
 $select = mysqli_query($sql,$conexao);
 
-$array = mysqli_fetch_row()
+$array = mysqli_fetch_array($select);
+
+$logarray = $array['login'];
+
+
+
+if($logarray == $login){
+
+    echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='../html/administracao-cadastro.html';</script>";
+
+    die();
+
+
+}else{
+
+    $query = "insert into usuarios (login,senha) values ('$login','$senha')";
+
+    $insert = mysqli_query($query,$conexao);
+    
+    if($insert){
+
+        echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='../html/administracao-cadastro.html';</script>";
+
+    }else{
+
+        echo"<script language='javascript' type='text/javascript'>alert('Não foi possivel cadastrar esse usúario');window.location.href='../html/administracao-cadastro.html';</script>";
+    }
+}
 
 
 
